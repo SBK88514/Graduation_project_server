3c916e9d22ba7eb7bb026395bfbe2d636765b119
Daniel <biton123654@gmail.com>
2024-12-08 10:42:37 +0200

    endpoints login

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 import express from "express";
-import {config} from "dotenv"
-config()
-import mongoDB from "./database/connectDB.js"
-mongoDB()
+import { config } from "dotenv";
+import mongoDB from "./database/connectDB.js";
+config();
+mongoDB();
 
 const app = express();
 app.use(express.json());
 
-import managerRouter from "./routers/manager.router.js";
+import usersRouter from "./routers/users.router.js";
 
-app.use("/manager", managerRouter);
+app.use("/users", usersRouter);
 
 const port = 3000;
 app.listen(port, () => console.log(`server is running on port ${port}`));
---
+++ routers/users.router.js
@@ -0,0 +1,31 @@
+import { Router } from "express";
+import queries from "../controllers/managers.controller.js";
+import queriesEmployees from "../controllers/employees.controller.js";
+
+const { signIn, signUp, update, deleteManager, getAllManagers } = queries;
+const {
+  employeeSignIn,
+  employeeSignUp,
+  validateEmail,
+  updateEmployee,
+  deleteEmployee,
+  getAllEmployees,
+} = queriesEmployees;
+const router = Router();
+
+//managers
+router.post("/manager/signin", signIn);
+router.post("/manager/signup", signUp);
+router.put("/manager/update/:id", update);
+router.delete("/manager/delete/:id", deleteManager);
+router.get("/manager/getallmanagers", getAllManagers);
+
+//employees
+router.post("/employee/signin", employeeSignIn);
+router.post("/employee/signup", employeeSignUp);
+router.get("/validationEmail/:id", validateEmail);
+router.put("/employee/update/:id", updateEmployee);
+router.delete("/employee/delete/:id", deleteEmployee);
+router.get("/manager/getallempolyees", getAllEmployees);
+
+export default router;
